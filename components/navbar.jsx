78247c5494cd80bb9b14bdefbeec062416b6acaa
Guilherme Loschiavo Alvarez <61693858+AlvarezGui@users.noreply.github.com>
2022-08-17 11:17:50 -0300

    Add files via upload

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -4,6 +4,7 @@ export default function Navbar(){
            
       <nav className='navbar navbar-expand-lg '>
         <div className='collapse navbar-collapse'>
+          <img width="30" height="30" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/gamemaker/gamemaker.png" alt="Logo" />
           <h1 className='navbar-brand'>Dispensa Virtual</h1>
           <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
             <li className='nav-item'><a className='nav-link active' href="/pagCadastrar">Apenas Cadastrar</a></li>
